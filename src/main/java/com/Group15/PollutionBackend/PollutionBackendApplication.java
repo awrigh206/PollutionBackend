f51c20c990d572c0230ec464a235d7ebae8a3982
Andrew <awrigh206@caledonian.ac.uk>
2020-01-29 00:26:05 +0000

    Initial commit, contains the files generated by the spring Intializr service

---
+++ src/main/java/com/Group15/PollutionBackend/PollutionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Group15.PollutionBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollutionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollutionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Group15/PollutionBackend/PollutionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group15.PollutionBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollutionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
