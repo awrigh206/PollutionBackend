48950858e3a9dd1488e5d9095bf38859dcc194e5
Andrew <awrigh206@caledonian.ac.uk>
2020-01-31 18:19:03 +0000

    Added avergaing period class

---
+++ src/main/java/com/Group15/PollutionBackend/Model/AveragingPeriod.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Group15.PollutionBackend.Model;
+
+/**
+ *
+ * @author Andrew Wright
+ */
+public class AveragingPeriod 
+{
+    private int value;
+    private String unit;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+    
+    
+}
