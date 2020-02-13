d5bdd9023bca955ac65594ea3afe822b0a434e76
Andrew <awrigh206@caledonian.ac.uk>
2020-02-13 10:39:09 +0000

    added a country model class

---
+++ src/main/java/com/Group15/PollutionBackend/Model/Country.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Group15.PollutionBackend.Model;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Andrew Wright
+ */
+@Entity
+public class Country implements Serializable 
+{
+    @GeneratedValue
+    @Id
+    private Integer id;
+    private String name;
+    private String countryCode;
+    @OneToMany
+    private List<City> citiesWithinCountry;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public List<City> getCitiesWithinCountry() {
+        return citiesWithinCountry;
+    }
+
+    public void setCitiesWithinCountry(List<City> citiesWithinCountry) {
+        this.citiesWithinCountry = citiesWithinCountry;
+    }
+    
+    public void addCity(City toAdd)
+    {
+        this.citiesWithinCountry.add(toAdd);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.name);
+        hash = 23 * hash + Objects.hashCode(this.countryCode);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Country other = (Country) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.countryCode, other.countryCode)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
