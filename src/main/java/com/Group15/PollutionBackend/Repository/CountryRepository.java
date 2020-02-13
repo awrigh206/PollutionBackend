7e58a2af6f6f41f7fb7971419fbdb5fb4986c9f1
Andrew <awrigh206@caledonian.ac.uk>
2020-02-13 10:43:12 +0000

    Country reposititory added

---
+++ src/main/java/com/Group15/PollutionBackend/Repository/CountryRepository.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Group15.PollutionBackend.Repository;
+
+import com.Group15.PollutionBackend.Model.City;
+import com.Group15.PollutionBackend.Model.Country;
+import java.util.ArrayList;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+/**
+ *
+ * @author Andrew Wright
+ */
+public interface CountryRepository extends PagingAndSortingRepository<Country,Integer> 
+{
+    @Override
+    public void deleteAll();
+
+    @Override
+    public void deleteAll(Iterable<? extends Country> itrbl);
+
+    @Override
+    public void delete(Country u);
+
+    @Override
+    public void deleteById(Integer id);
+
+    @Override
+    public <S extends Country> Iterable<S> saveAll(Iterable<S> itrbl);
+
+    @Override
+    public <S extends Country> S save(S s);
+    
+    Country findByName(@Param("name") String name);
+    ArrayList<Country> findAllByName(@Param("name") String name);
+    ArrayList<Country> findAllByCountryCode(@Param("code") String code);
+}
