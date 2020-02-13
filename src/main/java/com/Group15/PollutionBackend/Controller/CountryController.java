8b05baf24d62b7e9f94666998e67c44bee008bc2
Andrew <awrigh206@caledonian.ac.uk>
2020-02-13 17:48:42 +0000

    Added not found error message to the country controller

--- src/main/java/com/Group15/PollutionBackend/Controller/CountryController.java
+++ src/main/java/com/Group15/PollutionBackend/Controller/CountryController.java
@@ -9,10 +9,14 @@ import com.Group15.PollutionBackend.Model.City;
 import com.Group15.PollutionBackend.Model.Country;
 import com.Group15.PollutionBackend.Repository.CountryRepository;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -38,6 +42,13 @@ public class CountryController
     public Country getCity(@RequestParam(value ="countryCode")String countryCode)
     {
         return countryRepo.findByName(countryCode);
+    }
+    
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler (NoSuchElementException.class)
+    public String return400(NoSuchElementException ex)
+    {
+        return ex.getMessage();
     }
     
     
