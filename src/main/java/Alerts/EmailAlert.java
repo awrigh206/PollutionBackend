d3ead0ee6cf77820e45dda339bdbd475cb506ef4
Andrew <awrigh206@caledonian.ac.uk>
2020-02-19 15:48:31 +0000

    Added alert structure and added apache email dependency

---
+++ src/main/java/Alerts/EmailAlert.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Alerts;
+
+/**
+ *
+ * @author Andrew Wright
+ */
+public class EmailAlert implements IAlert
+{
+
+    @Override
+    public void sendAlert(String message) 
+    {
+        
+    }
+    
+}
---
+++ src/main/java/Alerts/IAlert.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Alerts;
+
+/**
+ *
+ * @author Andrew Wright
+ */
+public interface IAlert 
+{
+    public void sendAlert(String message);
+}
