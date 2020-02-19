1442ae7fa227dcaea79c04e550a34bb879a999d1
Andrew <awrigh206@caledonian.ac.uk>
2020-02-19 19:35:23 +0000

    added geolocation based on ip

--- src/main/java/com/Group15/PollutionBackend/Controller/AlertController.java
+++ src/main/java/com/Group15/PollutionBackend/Controller/AlertController.java
@@ -10,7 +10,13 @@ import Alerts.IAlert;
 import Alerts.TextAlert;
 import com.Group15.PollutionBackend.DTO.AlertDto;
 import com.Group15.PollutionBackend.DTO.UserDto;
+import com.Group15.PollutionBackend.Model.City;
 import com.Group15.PollutionBackend.StartupRunner;
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.model.CityResponse;
+import java.io.File;
+import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -20,6 +26,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -51,6 +58,26 @@ public class AlertController
     {
         alert = new TextAlert();
         alert.sendAlert(alertDto.getMessage(), alertDto.getAddress());
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, path = "/location")
+    public String getCity(@RequestParam(value ="ip")String ip)
+    {
+        try
+        {
+            File database = new File("src/main/resources/GeoLite2-City_20200211/GeoLite2-City.mmdb");
+            DatabaseReader dbReader = new DatabaseReader.Builder(database).build();
+            InetAddress ipAddress = InetAddress.getByName(ip);
+            CityResponse response = dbReader.city(ipAddress);
+            return response.getCity().getName();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+            
+        
     }
     
     @ResponseStatus(HttpStatus.NOT_FOUND)
