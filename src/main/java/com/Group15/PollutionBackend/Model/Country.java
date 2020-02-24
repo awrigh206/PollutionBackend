d0c64b890e15af645cd0fedbdf3a10ce42028b12
Andrew <awrigh206@caledonian.ac.uk>
2020-02-24 21:28:37 +0000

    Small performance improvements

--- src/main/java/com/Group15/PollutionBackend/Model/Country.java
+++ src/main/java/com/Group15/PollutionBackend/Model/Country.java
@@ -47,8 +47,8 @@ public class Country implements Serializable, IRepo
     public void fillInCityData( RetrieveData data)
     {
         //reset these defaults when you want to release the system. Set smaller values to decrease testing time
-        data.setLimit(10000);
-        int pageLimit = 1;
+        data.setLimit(1000);
+        int pageLimit = 2;
         //data.setLimit(20);
         //int pageLimit = 1;
         //RetrieveData data = new RetrieveData(1200);
@@ -60,6 +60,8 @@ public class Country implements Serializable, IRepo
             
             for(int i =1; i<totalPages+1;i++)
             {
+                if(i>pageLimit)
+                    break;
                 LocationResult result = (LocationResult)data.processPageSingle(url, i, LocationResult.class, countryCode);
                 for(Location current : result.getLocations())
                 {
@@ -70,8 +72,7 @@ public class Country implements Serializable, IRepo
                         citiesWithinCountry.add(current.toNewCity());
                 }
                 
-                if(i>pageLimit)
-                    break;
+                
                 
                 
             }
