879b750b912538f7d1e2e9609d9bb8a1d315e597
awrigh206 <awrigh206@caledonian.ac.uk>
2020-03-02 19:31:38 +0000

    No longer waits for all data to be added

--- src/main/java/com/Group15/PollutionBackend/StartupRunner.java
+++ src/main/java/com/Group15/PollutionBackend/StartupRunner.java
@@ -72,7 +72,7 @@ public class StartupRunner implements ApplicationListener<ContextRefreshedEvent>
                 t[i].start();
             }
             //add this back in for safety, can leave out for faster testing
-            waitForFinish(t);
+            //waitForFinish(t);
             long endTime = System.nanoTime();
             log.info("That took about: " + (endTime - beginTime));
             data.setLimit(1200);
