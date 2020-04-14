b49b87d9047d8ac4770e36b3abcc64c448ca2c0b
awrigh206 <awrigh206@caledonian.ac.uk>
2020-04-14 13:24:30 +0100

    Added location DTO

---
+++ src/main/java/com/Group15/PollutionBackend/DTO/LocationDto.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Group15.PollutionBackend.DTO;
+
+/**
+ *
+ * @author Andrew Wright
+ */
+public class LocationDto 
+{
+    private Double latitude;
+    private Double longitude;
+
+    public LocationDto(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+    
+    
+}
