0c8ac30cb8a511d873d7c0b09ae5218bf66d5aab
awrigh206 <awrigh206@caledonian.ac.uk>
2020-04-18 11:41:32 +0100

    Replaced missing semi-colon

--- src/main/java/com/Group15/PollutionBackend/Controller/UserController.java
+++ src/main/java/com/Group15/PollutionBackend/Controller/UserController.java
@@ -11,7 +11,7 @@ import com.Group15.PollutionBackend.Repository.UserRepository;
 import com.Group15.PollutionBackend.Service.UserService;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
